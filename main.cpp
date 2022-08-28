adb132fe4251af6f8f9270363a77c540dd060e80
Enemy89 <66246104+Enemy89@users.noreply.github.com>
2022-08-28 13:27:19 +0300

    rollback from 11.3 to 11.2

--- main.cpp
+++ main.cpp
@@ -3,61 +3,93 @@
 
 using namespace std;
 
-string IPv4;
-int address_part;
+string email;
 
-string octet (string ip, int partNumber) {
-    int count = 0;
-    string part;
-    for (int i = 0; i < ip.length(); i++) {
-        if (ip[i] == '.') {
-            count++;
-            if (count == partNumber) {
-                return part;
-            }
-            part="";
+string first_part_address(string line) {
+    string newLine;
+    for (int i=0; i<line.length(); i++) {
+        if (line[i]=='@') {
+            return newLine;
         }
-        else {
-            part += ip[i];
-        }
+        newLine+=line[i];
     }
 }
 
-string check_octet (string octet) {
-    string mark="false";
-    int octetInt = stoi(octet);
-    if (octet.length() > 0 && octet.length() < 4 && (octetInt>=0 && octetInt<=255)) {
-        for (int i = 0; i < octet.length(); i++) {
-            if (octet[i] >= '0' && octet[i] <= '9') {
-                mark="true";
-                 return mark;
-            }
-            else {
-                return mark;
+string second_part_address(string line) {
+    string newLine;
+    for (int i=line.length(); i>0; i--) {
+        if (line[i]=='@') {
+            return newLine;
+        }
+        newLine+=line[i];
+    }
+}
+
+string mirror_line(string line) {
+    string newLine;
+    for(int i=line.length(); i>0; i--) {
+        newLine+=line[i];
+    }
+    return newLine;
+}
+
+string before_symbol (string str) {
+    string result;
+    string checkStr = "'.!#$%&*+-/=?^_`{|}~QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890'\\'";
+    int checkGood=0, checkBad = 0;
+    if (str.length() < 1 || str.length() > 64) {
+        result = "No";
+        return result;
+    } else {
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < checkStr.length(); j++) {
+                if (str[i] == checkStr[j]) {
+                    checkGood++;
+                }
             }
         }
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str[0] == '.' || str[str.length()] == '.' || (str[i] == '.' && str[i + 1] == '.')) {
+                checkBad++;
+            }
+        }
+        return (checkGood == str.length() && checkBad == 0 ? "Yes" : "No");
+    }
+}
+
+string after_symbol(string str) {
+    string result;
+    string checkStr = "-.QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+    int checkGood = 0, checkBad = 0;
+    if (str.length() < 1 || str.length() > 63) {
+        result = "No";
+        return result;
     } else {
-        mark = "false";
-        return mark;
+        for (int i = 1; i < str.length(); i++) {
+            for (int j = 0; j < checkStr.length(); j++) {
+                if (str[i] == checkStr[j]) {
+                    checkGood++;
+                }
+            }
+        }
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str[0] == '.' || str[str.length()] == '.' || (str[i] == '.' && str[i + 1] == '.')) {
+                checkBad++;
+            }
+        }
+        return (checkGood == str.length()-1 && checkBad == 0 ? "Yes" : "No");
     }
 }
 
 int main() {
-    cout << "Enter IP and number octet:" << endl;
-    getline(cin, IPv4);
-    /*string one = octet(IPv4,1);
-    string two = octet(IPv4,2);
-    string three = octet(IPv4,3);
-    string four = octet(IPv4,4);
-    cout<<one<<endl<<two<<endl<<three<<endl<<four<<endl;
-    cout<<check_octet(octet(IPv4, 1))<<endl;*/
-    if (check_octet(octet(IPv4, 1)) == "true" && check_octet(octet(IPv4, 2)) == "true"
-        && check_octet(octet(IPv4, 3)) == "true" && check_octet(octet(IPv4, 4)) == "true") {
-        cout << "Valid"<<endl;
-    } else
-        cout << "Invalid"<<endl;
-     cout<<check_octet(octet(IPv4,1))<<endl;
-     cout<<check_octet(octet(IPv4,2))<<endl;
-     cout<<check_octet(octet(IPv4,3))<<endl;
-     cout<<check_octet(octet(IPv4,4))<<endl;
+    int count=0;
+    cout << "Enter email: " << endl;
+    getline(cin, email);
+    for (int i=0; i<email.length(); i++) {
+        if (email[i] == '@') {
+            count++;
+        }
+    }
+   cout<<(count!=1 ? "No email-address!" : (before_symbol(first_part_address(email))=="Yes" && after_symbol(mirror_line(
+           second_part_address(email)))=="Yes" ? "Yes" : "No") );
 }
